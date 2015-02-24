b50b20f76ead14bad4c0759bea0d1db4ae9825a0
Omer Hassan <theomer1@gmail.com>
2015-02-24 05:10:20 +0100

    116. Morse Code

---
+++ p116.py
@@ -0,0 +1,51 @@
+import sys
+
+CODE_TO_CHARACTER_MAP = {
+	'.-': 'A',
+	'-...': 'B',
+	'-.-.': 'C',
+	'-..': 'D',
+	'.': 'E',
+	'..-.': 'F',
+	'--.': 'G',
+	'....': 'H',
+	'..': 'I',
+	'.---': 'J',
+	'-.-': 'K',
+	'.-..': 'L',
+	'--': 'M',
+	'-.': 'N',
+	'---': 'O',
+	'.--.': 'P',
+	'--.-': 'Q',
+	'.-.': 'R',
+	'...': 'S',
+	'-': 'T',
+	'..-': 'U',
+	'...-': 'V',
+	'.--': 'W',
+	'-..-': 'X',
+	'-.--': 'Y',
+	'--..': 'Z',
+	'-----': '0',
+	'.----': '1',
+	'..---': '2',
+	'...--': '3',
+	'....-': '4',
+	'.....': '5',
+	'-....': '6',
+	'--...': '7',
+	'---..': '8',
+	'----.': '9'
+}
+
+def mapCodeToCharacter(code):
+	return CODE_TO_CHARACTER_MAP[code]
+
+for line in open(sys.argv[1]):
+	words = []
+
+	for word in [words.split(' ') for words in line.rstrip().split('  ')]:
+		words.append(''.join(map(mapCodeToCharacter, word)))
+
+	print(' '.join(words))
